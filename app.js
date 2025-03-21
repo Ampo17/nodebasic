b8a724d8cb864e49cf51460997a5a9b5ad36c93a
c9ampann <c9ampann@vasvari.org>
2025-03-21 12:12:41 +0100

    hibakezelés kész

--- app.js
+++ app.js
@@ -3,36 +3,51 @@ import express from "express";
 const app = express();
 app.use(express.json());
 
-const users = ["Jani", "Anna","Bob","Cedrik"];
+const users = ["Jani", "Anna", "Bob", "Cedrik"];
 
-app.get("/users", (req,res)=>{
-    res.status(200).json(users)
-})
+app.get("/users", (req, res) => {
+  res.status(200).json(users);
+});
 
-app.get("/users/:id", (req,res)=>{
-    const id= req.params.id;
-    res.status(200).json(users[id])
-})
+app.get("/users/:id", (req, res) => {
+  const id = req.params.id;
+  if (id < 0 || id >= users.length) {
+    return res.status(404).json({ message: "Nem található" });
+  }
+  res.status(200).json(users[id]);
+});
 
-app.post("/users", (req,res)=>{
-    const name = req.body.name;
-    users.push(name);
-    res.status(200).json(name);
-})
+app.post("/users", (req, res) => {
+  const name = req.body.name;
+  if (!name) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+  users.push(name);
+  res.status(200).json(name);
+});
 
-app.put("/users/:id", (req,res)=>{
-    const id= req.params.id;
-    const name = req.body.name;
-    users[id]=name
-    res.status(200).json(name);
-})
+app.put("/users/:id", (req, res) => {
+  const id = req.params.id;
+  if (id < 0 || id >= users.length) {
+    return res.status(404).json({ message: "Nem található" });
+  }
+  const name = req.body.name;
+  if (!name) {
+    return res.status(400).json({ message: "Invalid data" });
+  }
+  users[id] = name;
+  res.status(200).json(name);
+});
 
-app.delete("/users/:id", (req,res)=>{
-    const id = req.params.id;
-    users.splice(id,1);
-    res.status(200).json({message:"Sikeres törlés"})
-})
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id;
+  if (id < 0 || id >= users.length) {
+    return res.status(404).json({ message: "Not found" });
+  }
+  users.splice(id, 1);
+  res.status(200).json({ message: "Sikeres törlés" });
+});
 
-app.listen(3000, ()=> {
-    console.log("Müködik")
-})
+app.listen(3000, () => {
+  console.log("Müködik");
+});
